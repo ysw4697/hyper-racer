a0a8e3c0d92d778b8a0b0cb0fada778958943d22
ysw4697 <ysw4697@naver.com>
2025-01-23 11:36:29 +0900

    HR-6 자동차와 도로, 가스 모델 제작
    -빈 클래스 생성
    - 도로, 가스, 자동차 모델 제작

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
